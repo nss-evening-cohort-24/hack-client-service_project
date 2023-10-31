88271633501dc8d2915320066bc559634196ea94
MaggieChafee <maggie.chafee@gmail.com>
2023-10-30 21:54:54 -0500

    update functionality still needs help

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
